436f34434910acaab8244a492032dc4ac33bc13e
Nirmay <nirmay.singh.lamba@gmail.com>
2024-11-10 15:22:42 -0800

    Revert "use keywords for image search"
    This reverts commit cac990ecd3c9fd2a9d123fe473c99146db7de601.

--- src/api/lesson/controllers/lesson.js
+++ src/api/lesson/controllers/lesson.js
@@ -68,10 +68,9 @@ module.exports = createCoreController('api::lesson.lesson', ({ strapi }) => ({
         {
           role: "system",
           content: `You are a helpful assistant that divides lesson content into logical 
-          small bytes which can be easily consumed by a student to learn the concept. Give a small name to the lesson as well.  
-          Also for each section, we will search for images, so we need 2 words that describe the section to use for image search.         
+          small bytes which can be easily consumed by a student to learn the concept. Give a small name to the lesson as well.           
           The response should be a JSON. Don't use markdown. example format: 
-          {name: 'lesson name', sections: [{section_text: 'section 1', section_keywords: 'keyword1 keyword2']}, {section_text: 'section 2', section_keywords: 'keyword3 keyword4'}]}`
+          {name: 'lesson name', sections: [{section_text: 'section 1'}, {section_text: 'section 2'}]}`
         },
         {
           role: "user", 
@@ -93,7 +92,7 @@ module.exports = createCoreController('api::lesson.lesson', ({ strapi }) => ({
           'Authorization': 'gkMjrxkUs79lfW7bwto3erMyL9LUH73wcCKzl28pVJL5D0z9ytUQlLKB'
         },
         params: {
-          query: section.section_keywords,
+          query: section.section_text,
           size: 'small',
           per_page: 10
         }
